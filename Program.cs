46ca6d4c6cb5fc9c209530e65286e6cce06a41f0
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-09-12 14:32:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is registration for employee of K restaurant");
+            Console.WriteLine("enter employee's full name: ");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("enter employee's monthly salary: ");
+            string salary = Console.ReadLine();
+            Console.WriteLine($"{fullname} has sucessfully registered with monthly salary {salary}");
+        }
+    }
+}
